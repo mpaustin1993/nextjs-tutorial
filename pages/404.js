3a7e25cab88c5ab7ab1e3c327d5f0b8e18c4866c
michael_austin <austin.m@choiceenergymgt.com>
2021-11-17 15:52:36 -0600

    finished lesson-8

--- pages/404.js
+++ pages/404.js
@@ -5,6 +5,8 @@ import { useRouter } from 'next/router'
 const NotFound = () => {
   const router = useRouter()
 
+  // passing in [] as dependency array --> ensures that useEffect
+  // only fires onces when first mounted
   useEffect(() => {
     setTimeout(() => {
       // router.go(-1)
@@ -17,9 +19,9 @@ const NotFound = () => {
     <div className="not-found">
       <h1>Ooops...</h1>
       <h2>That page cannot be found :(</h2>
-      <p>Going back to the <Link href="/"><a>Homepage</a></Link> is 3 seconds...</p>
+      <p>Going back to the <Link href="/"><a>Homepage</a></Link> in 3 seconds...</p>
     </div>
   );
 }
- 
+
 export default NotFound;
